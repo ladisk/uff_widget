08d56470e4c5bcc63f6a196c8033eca6d57cf785
Lovro.3lar <Lovro.3lar@gmail.com>
2019-05-29 10:23:57 +0200

    adding imports

---
+++ uff_widget/__init__.py
@@ -0,0 +1,5 @@
+from .analyze_uff import analyze as analyze
+from .analyze_uff import get_info as get_info
+from .viz import viz as viz
+from .get_data import ds58 as get_data58
+from .get_data import ds55 as get_data55
