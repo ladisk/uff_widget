d138e6a3d1b3c17dda46e5869f9db99c97d07bec
Lovro.3lar <Lovro.3lar@gmail.com>
2019-06-03 13:15:07 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+     name='uff_widget',  
+     version='0.0.1',
+     scripts=['uff_widget'] ,
+     author="Lovro Trilar",
+     author_email="Lovro.3lar@gmail.com",
+     description="vizualization of uff file",
+     url="https://github.com/ladisk/uff_widget",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
