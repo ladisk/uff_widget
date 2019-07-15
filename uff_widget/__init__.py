c0c90a979cbf5e5b6bd92795fb70076892d20ab4
Lovro.3lar <Lovro.3lar@gmail.com>
2019-07-15 11:43:21 +0200

    migrating from function to class

--- uff_widget/__init__.py
+++
@@ -1,8 +0,0 @@
-from .read_uff import read_uff as read_uff
-from .read_uff import get_info as get_info
-from .show import show_3D as show_3D
-from .show import show_frf as show_frf
-from .get_data import ds58 as get_data58
-from .get_data import ds55 as get_data55
-from .get_data import dinfo55 as dinfo55
-from .get_data import dinfo58 as dinfo58
